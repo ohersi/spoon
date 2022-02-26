c9b55612c8ef39b5cbb89f77565cc1e8dd0131b0
Omar Hersi <o.hersi@yahoo.com>
2022-02-26 11:56:59 -0500

    added mock user data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import "./App.css";
+import { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
+// Context
+import UserContext from "./context/UserContext";
+// Components
 import Nav from "./components/Nav"
 import Nav2 from "./components/Nav2";
-import Login from "./pages/Login";
 import Main from "./components/Main";
-import { Routes, Route } from "react-router-dom";
+// Pages
+import Login from "./pages/Login";
+// MockUsers
+import mockUsers from './mockdata/MockUserData';
+// CSS
+import "./App.css";
+// Mui
 import { sizing } from '@mui/system'
 
 const App = () => {
+
+  const [user, setUser] = useState('')
+
+  console.log(mockUsers)
+  console.log(user)
   return (
-    <div className="App">
-       <Nav2 />
-      <Main />
-      <Login />
-      <Nav />
-    </div>
+    <>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Nav2 />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
+        <Nav />
+      </UserContext.Provider>
+    </>
+
   );
 }
 
