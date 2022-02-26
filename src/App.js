7927d5a0859bd82328c270505f4242f33075a1a9
Joshua Kelly <joshuakellyeng@gmail.com>
2022-02-26 12:27:29 -0500

    added routes and signup page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import "./App.css";
-import Nav from "./components/Nav"
-import Nav2 from "./components/Nav2";
-import Login from "./pages/Login";
-import Main from "./components/Main";
-import { Routes, Route } from "react-router-dom";
-import { sizing } from '@mui/system'
+import './App.css';
+import Nav from './components/Nav';
+import Nav2 from './components/Nav2';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  return (
-    <div className="App">
-      <Nav2 />
-      <Main />
-      <Login />
-      <Nav />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Nav2 />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<SignUp />} />
+			</Routes>
+			<Nav />
+		</div>
+	);
 }
 
 export default App;
