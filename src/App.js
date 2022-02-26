f6ad69687c72cb6b5d11f8d41081d57ec614bd9a
Omar Hersi <o.hersi@yahoo.com>
2022-02-26 08:19:24 -0500

    added mockdata folder and created mock user data file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import mockUsers from './mockdata/MockUserData';
 
-function App() {
+const App = () => {
+
+  console.log(mockUsers)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+      </div>
+    </>
   );
 }
 
