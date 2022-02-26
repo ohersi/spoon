f1fa78f0cc9814f225a96f26a962091dd310cd3f
Joshua Kelly <joshuakellyeng@gmail.com>
2022-02-26 09:55:10 -0500

    pages was capitalized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Nav from "./components/Nav";
-import Login from "./Pages/Login";
+import Login from "./pages/Login";
 import Main from "./components/Main";
 import { Routes, Route } from "react-router-dom";
 function App() {
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -24,8 +24,4 @@ export default function SimpleBottomNavigation() {
       </BottomNavigation>
     </Box>
   );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1dfa3a33ba3792efe66b24cbedd44b05376775bf
